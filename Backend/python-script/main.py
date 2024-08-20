721d0e7432ad1a6e9d00178a28ff2a594b1924db
Suyash Mittal <44423507+suyashmittal@users.noreply.github.com>
2024-08-20 23:39:06 +0530

    Update main.py

--- Backend/python-script/main.py
+++ Backend/python-script/main.py
@@ -14,10 +14,9 @@ predicted_probabilities = model.predict_proba(word_features)
 
 top_3_indices = predicted_probabilities.argsort(axis=1)[:, -3:][:, ::-1]
 top_3_categories = model.classes_[top_3_indices]
-top_3_probabilities = predicted_probabilities[0, top_3_indices[0]]
 
 result = []
 for j in range(3):
-    result.append({"Category": top_3_categories[0, j], "Probability": top_3_probabilities[j]})
+    result.append({"Category": top_3_categories[0, j]})
 
 print(result)
